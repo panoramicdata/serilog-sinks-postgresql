84e5c481ba062e6c7b02031b4404152217111c42
Ecstatik <johankool2003@hotmail.co.uk>
2023-08-16 13:13:13 +0100

    Codacy Fixes

--- Serilog.Sinks.PostgreSql/DefaultColumnNames.cs
+++ Serilog.Sinks.PostgreSql/DefaultColumnNames.cs
@@ -2,16 +2,16 @@
 {
 	public static class DefaultColumnNames
 	{
-		public static string RenderedMesssage = "message";
+		public static readonly string RenderedMesssage = "message";
 
-		public static string MessageTemplate = "message_template";
+		public static readonly string MessageTemplate = "message_template";
 
-		public static string Level = "level";
+		public static readonly string Level = "level";
 
-		public static string Timestamp = "timestamp";
+		public static readonly string Timestamp = "timestamp";
 
-		public static string Exception = "exception";
+		public static readonly string Exception = "exception";
 
-		public static string LogEventSerialized = "log_event";
+		public static readonly string LogEventSerialized = "log_event";
 	}
 }
